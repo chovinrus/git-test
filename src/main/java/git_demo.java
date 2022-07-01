53a9f832d057d3fd07731a736bda864a884765b6
lindiayv <2330645526@qq.com>
2022-07-01 09:51:11 +0800

    commit 3.0 by hot_fix

--- src/main/java/git_demo.java
+++ src/main/java/git_demo.java
@@ -6,5 +6,6 @@ public class git_demo {
     public static void main(String[] args) {
         System.out.println("hello world!");
         int num = 0;
+        System.out.println("hello world!hot_fix");
     }
 }
